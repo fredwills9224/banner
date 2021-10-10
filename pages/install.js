b3c80205a120775a721500edb403d09194c4fd17
fredwills9224 <willsf9224@gmail.com>
2021-10-10 17:03:13 -0400

    toggle install script tag

--- pages/install.js
+++ pages/install.js
@@ -1,14 +1,43 @@
-import { Page } from "@shopify/polaris";
-import React from "react";
+import { Layout, Page, SettingToggle, TextStyle } from "@shopify/polaris";
+import React, { useState } from "react";
 
 function install(){
+
+    const [isInstalled, setIsInstalled] = useState(null);
+    const titleDescription = isInstalled ? 'Uninstall' : 'Install';
+    const bodyDescription = isInstalled ? 'installed' : 'uninstalled';
+    function handleAction(){
+        setIsInstalled(oldValue => !oldValue);
+    };
     return(
 
         <Page>
-            Install page
+            <Layout.AnnotatedSection
+                title={`${titleDescription} banner`}
+                description='Toggle banner installation on your shop'
+            >
+                <SettingToggle
+                    action={{
+                        content: titleDescription,
+                        onAction: handleAction
+                    }}
+                    enabled={true}
+                >
+
+                    The banner script is {' '} 
+                    <TextStyle variation='strong'>{bodyDescription}</TextStyle>
+
+                </SettingToggle>
+            </Layout.AnnotatedSection>
         </Page>
 
     );
+    
 };
 
 export default install;
+
+
+
+
+
