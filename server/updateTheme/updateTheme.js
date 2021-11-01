8deff9a75a0986f121f98a7ac4bba5a76ff4054a
fredwills9224 <willsf9224@gmail.com>
2021-10-31 21:44:33 -0400

    testing the fetch and add snippet into liquid page

--- server/updateTheme/updateTheme.js
+++ server/updateTheme/updateTheme.js
@@ -1,8 +1,10 @@
 import Axios from 'axios';
+import fs from 'fs';
+import path from 'path';
 
 const themeApi = 'admin/api/2021-01';
 
-export function updateTheme(shop, accessToken){
+export async function updateTheme(shop, accessToken){
 
     const axios = Axios.create({
         baseURL: `https://${shop}/${themeApi}`,
@@ -11,7 +13,33 @@ export function updateTheme(shop, accessToken){
             'X-Shopify-Access-Token': accessToken
         }
     });
-    getThemeId(axios);
+    const mainThemeId = await getThemeId(axios);
+    if(!mainThemeId){
+        return;
+    }
+    getAssetThemeLiquid(mainThemeId, axios);
+
+}
+
+async function getAssetThemeLiquid(id, axios){
+
+    const { data } = await axios.get(`/themes/${id}/assets.json?asset[key]=layout/theme.liquid`);
+    console.log('Theme liquid file');
+    if(!data.asset.value){
+        return;
+    }
+    const snippet = fs.readFileSync(path.resolve(__dirname, '../../liquid/theme.liquid'));
+    let newPage = data.asset.value;
+    if(newPage.includes(snippet)){
+        console.log('Page already has the snippet installed');
+        return;
+    }
+    newPage = data.asset.value.replace(
+        `{% section 'header' %}`,
+        `{% section 'header' %}\n${snippet}\n`
+    );
+    console.log('New Page', newPage);
+    return newPage;
 
 }
 
@@ -26,5 +54,6 @@ async function getThemeId(axios){
     }
     console.log('The main theme is:', mainTheme);
     console.log('The main theme id is:', mainTheme.id);
+    return mainTheme.id;
 
 }
